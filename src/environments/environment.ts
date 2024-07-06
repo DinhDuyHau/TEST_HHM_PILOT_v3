6c6c13beeba3ec102765bf97b935a00809e37ca0
tuananhnl <tuananhnl@gmail.com>
2024-07-06 09:38:56 +0700

    merge retail

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,11 @@
 export const environment = {
     production: false,
 
-    apiUrl: 'https://test-hhm-ketnoi.genbyte.net',
-    accountingUrl: 'http://test-hhm-ketoan.genbyte.net', // link 130
+    // apiUrl: 'https://test-hhm-ketnoi.genbyte.net',
+    apiUrl: 'http://localhost:5000',
+    // accountingUrl: 'http://test-hhm-ketoan.genbyte.net', // link 130
+    accountingUrl: 'http://hhm-qt.genbyte.net', // link 115
+
 
     //Khóa công khai của người nhận
     receiverPublicKey: `-----BEGIN PUBLIC KEY-----
