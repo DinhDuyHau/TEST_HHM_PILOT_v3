65234b466c3ba7f325bb580bcfbaa3079ff2406c
zPhanTrung <53164611+zPhanTrung@users.noreply.github.com>
2024-07-24 23:00:06 +0700

    update otp input

--- src/app/sales-management/component/form-control-custom/form-payment-custom/form-payment-custom.component.ts
+++ src/app/sales-management/component/form-control-custom/form-payment-custom/form-payment-custom.component.ts
@@ -131,8 +131,6 @@ export class FormPaymentCustomComponent implements OnChanges, OnInit {
       return dataFormatPipe.transform(value, this.dataType, format);
     } else if (this.dataType === 'number') {
       return value.toString().replace(/\D/g, '') || '0';
-    } else if (this.dataType === 'text-number') {
-      return value.toString().replace(/\D/g, '')
     }
     else {
       return value;
