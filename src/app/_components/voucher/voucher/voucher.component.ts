5b99a2623602e4810e4b83d1472a201579032af1
zPhanTrung <53164611+zPhanTrung@users.noreply.github.com>
2024-08-06 01:05:16 +0700

    update

--- src/app/_components/voucher/voucher/voucher.component.ts
+++ src/app/_components/voucher/voucher/voucher.component.ts
@@ -4,12 +4,13 @@ import { Button, Field, Grid, IGridServiceV2 } from '../../gridV2/grid.model';
 import { VoucherService } from './voucher.service';
 import { TypeVoucher, VOUCHER_TYPE } from '../enum/voucher_enum';
 import button from '@app/_common/button';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { AuthenticationService } from '@app/_services';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatDialog } from '@angular/material/dialog';
 import { HttpClient } from '@angular/common/http';
 import { CommonService } from '@app/sales-management/page/common/common.service';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-voucher',
@@ -22,7 +23,7 @@ export class VoucherComponent extends Grid<Model> implements OnInit, OnChanges {
   buttonsCustom: Button[] = [];
   voucherService!: VoucherService;
 
-  constructor(private http: HttpClient, private dialog: MatDialog, private authenticateService: AuthenticationService, private snack: MatSnackBar, private route: ActivatedRoute, private commonService: CommonService) {
+  constructor(private http: HttpClient, private router: Router, private dialog: MatDialog, private authenticateService: AuthenticationService, private snack: MatSnackBar, private route: ActivatedRoute, private commonService: CommonService) {
     localStorage.removeItem('useGridCached');
 
     const voucherService = new VoucherService(http, dialog, authenticateService, snack, route, commonService);
@@ -37,7 +38,14 @@ export class VoucherComponent extends Grid<Model> implements OnInit, OnChanges {
     document.title = voucherService.getTitle();
   }
   ngOnInit(): void {
-    //
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      filter((event: any) => {
+        return (this.route?.snapshot as any)['_routerState']?.url === event.url;
+      })
+    ).subscribe((event) => {
+      this.loadData({ pageIndex: this.pageIndex, pageSize: this.pageSize });
+    });
   }
 
   ngOnChanges(): void {
