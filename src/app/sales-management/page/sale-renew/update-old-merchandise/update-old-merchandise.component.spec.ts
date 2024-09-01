44ed586f9f5ceaa90e9d54b52660a7e45ea104b5
duyhau <103493705+DinhDuyHau@users.noreply.github.com>
2024-09-01 10:07:38 +0700

    Update update-old-merchandise.component.spec.ts

--- src/app/sales-management/page/sale-renew/update-old-merchandise/update-old-merchandise.component.spec.ts
+++ src/app/sales-management/page/sale-renew/update-old-merchandise/update-old-merchandise.component.spec.ts
@@ -1,1 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UpdateOldMerchandiseComponent } from './update-old-merchandise.component';
+
+describe('UpdateOldMerchandiseComponent', () => {
+  let component: UpdateOldMerchandiseComponent;
+  let fixture: ComponentFixture<UpdateOldMerchandiseComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UpdateOldMerchandiseComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(UpdateOldMerchandiseComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
 
