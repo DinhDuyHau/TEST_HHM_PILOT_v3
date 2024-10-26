2cb280e296ebfcfc4eca344ed02339c42a2e3858
tuananhnl <tuananhnl@gmail.com>
2024-10-26 19:20:56 +0700

    up phiếu mua lại, httt quẹt thẻ

--- src/app/sales-management/page/retail/retail.component.ts
+++ src/app/sales-management/page/retail/retail.component.ts
@@ -365,6 +365,7 @@ export class RetailComponent implements OnInit, AfterViewInit {
     const merchandise = this.merchandiseService.getMerchandiseNotHaveImei(merchandiseResponse.ma_vt, this.ticket.merchandise);
     merchandise && (merchandise.ma_imei = merchandiseResponse.ma_imei) && (merchandise.ma_kho = merchandiseResponse.ma_kho);
 
+    console.log(merchandiseResponse)
     if (!merchandise) {
       this.merchandiseService.addNew(merchandiseResponse, this.ticket.merchandise, Merchandise);
       this.handleCheckDeposit(merchandiseResponse.ma_vt, true);
