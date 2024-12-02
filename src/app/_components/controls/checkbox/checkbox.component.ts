654b2079d1b6750db5d9cc5bd10c3d6708540b9a
DinhDuyHau <duyhau2khn@gmail.com>
2024-12-02 14:14:26 +0700

    update checkbox

--- src/app/_components/controls/checkbox/checkbox.component.ts
+++ src/app/_components/controls/checkbox/checkbox.component.ts
@@ -3,7 +3,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-checkbox',
   templateUrl: './checkbox.component.html',
-  styleUrls: ['./checkbox.component.css']
+  styleUrls: ['./checkbox.component.scss']
 })
 export class CheckboxComponent {
   @Input() value: boolean = true;
