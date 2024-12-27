7410bdb16888824494a931e7f240ebd0c35b9daf
Nguyễn Duy Dự <nduydu66@gmail.com>
2024-12-27 11:21:26 +0700

    fix lỗi message báo object

--- src/app/sales-management/component/form-control-custom/form-input-custom/form-input-custom.component.ts
+++ src/app/sales-management/component/form-control-custom/form-input-custom/form-input-custom.component.ts
@@ -24,6 +24,7 @@ import { CommonService } from '@app/sales-management/page/common/common.service'
 })
 export class FormInputCustomComponent implements OnChanges, OnInit {
   @Output() handleEnterInput = new EventEmitter<any>();
+  @Output() enterKeyPressed = new EventEmitter<any>();
   @Output() handleClickSearch = new EventEmitter<any>();
   @Output() handleClickOpenDialog = new EventEmitter<any>();
   @Output() handleClickUploadImage = new EventEmitter<any>();
@@ -140,7 +141,7 @@ export class FormInputCustomComponent implements OnChanges, OnInit {
       //this.focusNext(event.target);
       //this.moveToNext(event);
       this.handleEnterInput.emit(value);
-      this.handleEnterInput.emit(event);
+      this.enterKeyPressed.emit(event);
     }
   }
 
