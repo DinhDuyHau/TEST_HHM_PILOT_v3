87c51a40f1bcec8c1b9dd5cdd0d891a53a26859c
Nguyễn Duy Dự <nduydu66@gmail.com>
2025-01-16 20:29:04 +0700

    update xóa ck 09

--- src/app/sales-management/page/sale-online/hh-web/sale-online.component.ts
+++ src/app/sales-management/page/sale-online/hh-web/sale-online.component.ts
@@ -890,7 +890,9 @@ export class SaleOnlineComponent implements OnInit, AfterViewInit {
         const merchandise = event.item as any;
         const ma_imei = merchandise.ma_imei || '';
 
-        this.ticket.discount = this.ticket.discount.filter(item => item.loai_ck !== DISCOUNT_TYPE.DISCOUNT_CUSTOMER_RANK && item.ma_imei.trim().toLowerCase() !== ma_imei.trim().toLowerCase());
+        this.ticket.discount = this.ticket.discount.filter(item => !(item.loai_ck === DISCOUNT_TYPE.DISCOUNT_CUSTOMER_RANK
+          && item.ma_imei && item.ma_imei.trim().toLowerCase() === ma_imei.trim().toLowerCase())
+        );
 
         // set bằng rỗng ma_hang
         this.ticket.masterInfo.ma_hang = '';
