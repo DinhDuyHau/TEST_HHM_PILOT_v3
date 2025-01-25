e66a47070bdb23c1b0d60f1730ce80f361b8a2b1
Nguyễn Duy Dự <nduydu66@gmail.com>
2025-01-25 10:15:08 +0700

    kiểm tra độ dài diễn dải trước khi lưu

--- src/app/sales-management/page/sale-renew/sale-renew.service.ts
+++ src/app/sales-management/page/sale-renew/sale-renew.service.ts
@@ -540,6 +540,9 @@ export class SaleRenewService {
         else if (ticket.discount.find(x => x.loai_ck == DISCOUNT_TYPE.REDUTION_FOR_CUSTOMER) && ((!ticket.masterInfo.nguoi_duyet_ck) || (ticket.masterInfo.nguoi_duyet_ck.trim() === ''))) {
             message = this.commonService.getMessage('lbl_invalid_ck04');
         }
+        else if (ticket.masterInfo.dien_giai.length > 250) {
+            message = 'Diễn giải không được vượt quá 250 ký tự';
+        }
         return message;
     }
 
