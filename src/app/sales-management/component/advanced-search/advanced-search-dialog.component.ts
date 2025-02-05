00d88c35174b03f971035242cf74c18c97e24923
Nguyễn Duy Dự <nduydu66@gmail.com>
2025-02-05 14:09:55 +0700

    thêm filter cho mã kho nhâp, xuất

--- src/app/sales-management/component/advanced-search/advanced-search-dialog.component.ts
+++ src/app/sales-management/component/advanced-search/advanced-search-dialog.component.ts
@@ -358,7 +358,8 @@ export class AdvancedSearchDialogComponent implements OnInit {
       keyword: ma_vt || '',
       stockData: this.getDataStock(ma_kho),
       selectedStock: selectedStock,
-      componentName: SEARCH_V2_COMPONENT_NAME.WAREHOUSE
+      componentName: SEARCH_V2_COMPONENT_NAME.WAREHOUSE,
+      isFilter: true
     }, 'search-style-dialog')
       .afterClosed().subscribe(result => {
         if (result) {
