a71f15d680d0771daca74d480055db08736cb8a6
Nguyễn Duy Dự <nduydu66@gmail.com>
2025-02-07 10:39:56 +0700

    thêm kho TL khi lọc

--- src/app/_components/voucher/stock-transfer/from-shop/stock-transfer-from-shop.component.ts
+++ src/app/_components/voucher/stock-transfer/from-shop/stock-transfer-from-shop.component.ts
@@ -263,7 +263,7 @@ export class StockTransferFromShopComponent implements OnInit, AfterViewInit {
         // được phép điều chuyển về 2 loại kho: hàng cũ, trôi bảo hành
         if (this.ma_loai === 'HC' || this.ma_loai === 'TL') {
           stock_operator = 'in'
-          ma_loai = 'HC,TBH';
+          ma_loai = 'HC,TBH,TL';
         }
         // hàng trải nghiệm
         // được phép điều chuyển về các loại kho: hàng cũ, trôi bảo hành, hàng trải nghiệm
