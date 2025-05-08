76fd916efd3590107a8a03b1b15e74d6fb4d413c
tuananhnl <tuananhnl@gmail.com>
2025-05-08 15:43:50 +0700

    phiếu nhập TL: thêm gd "trả lại GH không thu tiền"

--- src/app/sales-management/api/imei-api.service.ts
+++ src/app/sales-management/api/imei-api.service.ts
@@ -107,13 +107,13 @@ export class ImeiApiService extends ApiService {
         }
     }
 
-    getSoldInfoReturn(ma_imei: string, ma_cuahang: string, ma_ct = '', rate = -1, tien_giam = 0, loai_tra_lai = '', tra_lai_cod = false): Observable<ResultNoPaging<Imei>> {
+    getSoldInfoReturn(ma_imei: string, ma_cuahang: string, ma_ct = '', rate = -1, tien_giam = 0, loai_tra_lai = '', tra_lai_cod = false, tra_lai_freedelivery = false): Observable<ResultNoPaging<Imei>> {
         ma_imei = encodeURIComponent(ma_imei);
         if (rate == -1 && tien_giam == 0) {
-            return this.get<ResultNoPaging<Imei>>(GET_SOLD_INFO_RETURN_IMEI_URL, { ma_imei, ma_cuahang, ma_ct, loai_tra_lai, tra_lai_cod });
+            return this.get<ResultNoPaging<Imei>>(GET_SOLD_INFO_RETURN_IMEI_URL, { ma_imei, ma_cuahang, ma_ct, loai_tra_lai, tra_lai_cod, tra_lai_freedelivery });
         }
         else {
-            return this.get<ResultNoPaging<Imei>>(GET_SOLD_INFO_RETURN_IMEI_URL, { ma_imei, ma_cuahang, ma_ct, rate, tien_giam, loai_tra_lai, tra_lai_cod });
+            return this.get<ResultNoPaging<Imei>>(GET_SOLD_INFO_RETURN_IMEI_URL, { ma_imei, ma_cuahang, ma_ct, rate, tien_giam, loai_tra_lai, tra_lai_cod, tra_lai_freedelivery });
         }
     }
 
--- src/app/sales-management/model/ticket/sale-return/model.ts
+++ src/app/sales-management/model/ticket/sale-return/model.ts
@@ -31,6 +31,7 @@ export class MasterInfo extends BaseMasterInfo {
   t_da_tra = 0;
   stt_rec_hd = '';
   tra_lai_cod = false;
+  tra_lai_freedelivery = false;
   so_dh_vc = ''; // mã đơn hàng
   ma_nvvc = ''; // mã đơn vị vận chuyển
   ma_van_don = ''; // mã vận đơn
