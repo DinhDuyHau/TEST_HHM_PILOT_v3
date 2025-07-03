82ddfb7fc012b272c01388650a5f39dc71aed4dc
Nguyễn Duy Dự <nduydu66@gmail.com>
2025-07-03 09:53:57 +0700

    bổ sung httt mbqr cho các phiếu thu

--- src/app/_components/voucher/money-transaction/other-receipt/create/create.component.ts
+++ src/app/_components/voucher/money-transaction/other-receipt/create/create.component.ts
@@ -87,8 +87,7 @@ export class OtherReceiptDetailComponent extends Grid<ReceiptDetail> implements 
     'giam_gia_crm',
     'chiet_khau',
     'voucher_doi_tac',
-    'quet_the_tra_gop',
-    'mb_qr'
+    'quet_the_tra_gop'
   ];
 
 
