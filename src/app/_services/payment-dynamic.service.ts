c42182c948a4fd2da0ea8c33e82b2e52f379d753
tuananhnl <tuananhnl@gmail.com>
2025-08-21 09:49:56 +0700

    MBQR: không gọi trực tiếp Bank API, gọi từ HHM-API

--- src/app/_services/payment-dynamic.service.ts
+++ src/app/_services/payment-dynamic.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { environment } from '@environments/environment.prod';
+import { environment } from '@environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class PaymentDynamicService {
@@ -9,17 +9,26 @@ export class PaymentDynamicService {
     constructor(private http: HttpClient) { }
 
     createQrCode(body: any, paymenyCode: string) {
-        let url = `${environment.apiBankUrl}/${this.versionAPI}/Payment/${paymenyCode}/createqr`;
+        // let url = `${environment.apiBankUrl}/${this.versionAPI}/Payment/${paymenyCode}/createqr`;
+        // return this.http.post<any>(url, body);
+
+        let url = `${environment.apiUrl}/QrPayment/createqr/${paymenyCode}`;
         return this.http.post<any>(url, body);
     }
 
     getRefCode(paymenyCode: string) {
-        let url = `${environment.apiBankUrl}/${this.versionAPI}/Payment/${paymenyCode}/getRefCode`;
+        // let url = `${environment.apiBankUrl}/${this.versionAPI}/Payment/${paymenyCode}/getRefCode`;
+        // return this.http.get<any>(url);
+
+        let url = `${environment.apiUrl}/QrPayment/getrefcode/${paymenyCode}`;
         return this.http.get<any>(url);
     }
 
     deleteQrCode(body: any, paymenyCode: string) {
-        let url = `${environment.apiBankUrl}/${this.versionAPI}/Payment/${paymenyCode}/deleteqr`;
+        // let url = `${environment.apiBankUrl}/${this.versionAPI}/Payment/${paymenyCode}/deleteqr`;
+        // return this.http.delete<any>(url, { body: body });
+
+        let url = `${environment.apiUrl}/QrPayment/deleteqr/${paymenyCode}`;
         return this.http.delete<any>(url, { body: body });
     }
 }
