2a1203d34a17351379cabcfde740cbcbae2b3111
Duy Hau <duyhau2khn@gmail.com>
2025-09-27 12:20:04 +0700

    update: Phiếu đổi hàng

--- src/app/sales-management/page/sale-change/sale-change.component.ts
+++ src/app/sales-management/page/sale-change/sale-change.component.ts
@@ -581,7 +581,10 @@ export class SaleChangeComponent implements OnInit, AfterViewInit {
 
   onRemoveServiceChange($event: any) {
     const itemRemove = $event.item;
-    this.ticket.service = this.ticket.service.filter(item => item.ma_dv !== itemRemove.ma_dv);
+    //this.ticket.service = this.ticket.service.filter(item => item.ma_dv !== itemRemove.ma_dv);
+    this.ticket.service = this.ticket.service.filter(item =>
+      !(item.ma_dv === itemRemove.ma_dv && item.line_nbr === itemRemove.line_nbr)
+    );
   }
 }
 
