287d1d3e22f824c82933f73b86eb27662489e837
tuananhnl <tuananhnl@gmail.com>
2025-10-19 22:51:28 +0700

    BHK: Thêm button phát hành hddt

--- src/app/sales-management/page/sale-renew/sale-renew.component.ts
+++ src/app/sales-management/page/sale-renew/sale-renew.component.ts
@@ -142,6 +142,7 @@ export class SaleRenewComponent implements OnInit, AfterViewInit {
   isGetPdfInvoice = false;
   invoice_model_status = '0';
   current_renew_item: any = null;
+  isPublistEInvoice = false;
 
   tab_sources: any[] = [
     { label: 'Tổng quan' },
@@ -2037,6 +2038,45 @@ export class SaleRenewComponent implements OnInit, AfterViewInit {
     });
   }
 
+  onPublishEInvoice() {
+    //check trạng thái phiếu
+    if (this.invoice_model_status !== '2') {
+      this.commonService.showMessageByName('Phiếu chưa hoàn thành, không thể phát hành HĐĐT.');
+      return;
+    }
+
+    //check quyền sys admin
+    if (!this.allowAdminEdit()) {
+      console.log('Không phải tk sysadmin');
+      return;
+    }
+
+    let title = `Thực hiện phát hành HĐĐT cho phiếu số: ${this.ticket.masterInfo.so_ct}?`;
+    this.commonService.openDialog(DialogConfirmComponent, { title: title })
+      .afterClosed().subscribe(result => {
+        if (result) {
+          this.isPublistEInvoice = true;
+          this.internalSaleDeatailService.publishInvoiceBySysAdmin(this.ticket).subscribe({
+            next: (result: any) => {
+              if (result.success) {
+                this.commonService.showMessageByName(result.message || 'create_publish_invoice_success');
+              } else {
+                this.commonService.showMessageByName(result.message || 'Unknown_err');
+              }
+            },
+            error: (err) => {
+              this.commonService.showMessageByName('Unknown_err');
+              console.error('Draft invoice error:', err);
+              this.isPublistEInvoice = false;
+            },
+            complete: () => {
+              this.isPublistEInvoice = false;
+            }
+          });
+        }
+      });
+  }
+
   //#endregion
 
   //#region Readonly
@@ -2084,6 +2124,12 @@ export class SaleRenewComponent implements OnInit, AfterViewInit {
 
   isAnyPaymentSelected(): boolean {
     return Object.values(this.ticket.payment).some(p => p?.selected === true);
+  }
+
+  allowAdminEdit(): boolean {
+    const user_authorization = JSON.parse(localStorage.getItem('authorization')!);
+
+    return user_authorization && user_authorization.sa_yn;
   }
   //#endregion
 
