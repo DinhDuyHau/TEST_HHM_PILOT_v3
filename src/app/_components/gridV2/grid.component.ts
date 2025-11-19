65986beca28ffe933da87cc915dcd72dd006bff2
Duy Hau <duyhau2khn@gmail.com>
2025-11-19 17:06:40 +0700

    update khai báo bỏ các chức báo cáo

--- src/app/_components/gridV2/grid.component.ts
+++ src/app/_components/gridV2/grid.component.ts
@@ -118,7 +118,7 @@ export class GridV2Component implements AfterViewInit, OnInit, OnChanges {
     QUICK_SEARCH: 2
   };
   statusList: StatusTicket[] = [];
-  disableButtonExports = ['rptLookupInventory', 'rptRetailPrice']; // khai báo sysid những báo cáo ko hiển thị nút kết xuất
+  //disableButtonExports = ['rptLookupInventory']; // khai báo sysid những báo cáo ko hiển thị nút kết xuất
   isViewButtonExport = true; // kiểm tra có được hiển thị nút kết xuất hay ko
 
   selected_row_item: any;
@@ -159,9 +159,9 @@ export class GridV2Component implements AfterViewInit, OnInit, OnChanges {
         this.statusList = result;
       });
     }
-    if (this.disableButtonExports.includes(this.sysid)) {
-      this.isViewButtonExport = false;
-    }
+    // if (this.disableButtonExports.includes(this.sysid)) {
+    //   this.isViewButtonExport = false;
+    // }
   }
   ngAfterViewInit(): void {
     // console.log(this.fields);
