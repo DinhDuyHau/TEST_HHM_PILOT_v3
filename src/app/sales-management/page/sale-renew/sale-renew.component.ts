95438132fb1d08dc80a064bcf24b8ea065d3759f
Duy Hau <duyhau2khn@gmail.com>
2025-11-28 11:17:52 +0700

    update fix lỗi hạng thành viên phiếu bán online-website và phiếu thu cũ đổi mới

--- src/app/sales-management/page/sale-renew/sale-renew.component.ts
+++ src/app/sales-management/page/sale-renew/sale-renew.component.ts
@@ -398,7 +398,18 @@ export class SaleRenewComponent implements OnInit, AfterViewInit {
         .afterClosed().subscribe(result => {
           if (!result) {
             //không xác nhận => reset về mã cũ
-            this.ticket.masterInfo.ma_kh = ma_kh_old;
+            this.customerApiService.getOneById(ma_kh_old).subscribe(result => {
+              if (result.success && result.result) {
+                const customer: any = result.result;
+                this.saleRenewService.setInfoCustomer(customer);
+                this.ticketApiService.getRankCustomer({ ma_kh: customer.ma_kh }).subscribe(result => {
+                  const { ma_hang, mau_chu, tl_tich_diem } = result.result as any;
+                  this.ticket.masterInfo.ma_hang = ma_hang || '';
+                  this.ticket.masterInfo.tl_tich_diem = tl_tich_diem || 0;
+                  this.generateLabelWithColor(ma_hang, mau_chu);
+                });
+              }
+            });
             return;
           }
           // xóa và reset ck 09 khi xác nhận => reset
